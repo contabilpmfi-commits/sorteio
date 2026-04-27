f2bc2e4719c5855c2875d35fa48d76a0eb828e55
contabilpmfi-commits <contabil.pmfi@gmail.com>
2026-04-27 15:52:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,26 @@
-const CACHE = "app-v1";
+const CACHE = "app-v2"; // mudou versão
 
 self.addEventListener("install", e=>{
+    self.skipWaiting();
+
     e.waitUntil(
         caches.open(CACHE).then(cache=>{
             return cache.addAll([
                 "./",
                 "./index.html"
             ]);
+        })
+    );
+});
+
+self.addEventListener("activate", e=>{
+    e.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(
+                keys.map(k=>{
+                    if(k !== CACHE) return caches.delete(k);
+                })
+            );
         })
     );
 });
