dc0a4bf3b246076580b730136f49b219804e39b6
contabilpmfi-commits <contabil.pmfi@gmail.com>
2026-04-27 15:29:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,12 @@
-const CACHE = "consorcio-app-v1";
+const CACHE = "app-v1";
 
-self.addEventListener("install", e => {
+self.addEventListener("install", e=>{
     e.waitUntil(
-        caches.open(CACHE).then(cache => {
+        caches.open(CACHE).then(cache=>{
             return cache.addAll([
                 "./",
-                "./index.html",
-                "./css/style.css",
-                "./js/app.js",
-                "./img/Logo.png"
+                "./index.html"
             ]);
         })
-    );
-});
-
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
     );
 });
