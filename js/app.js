6059196d3cb263975cbba5933dd457509acfa167
contabilpmfi-commits <contabil.pmfi@gmail.com>
2026-04-27 15:46:14 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,43 +2,32 @@ import { auth } from "./firebase.js";
 
 import {
   signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 /* TROCA DE TELA */
 function mostrar(id){
-    ["login","dashboard"].forEach(t=>{
+    ["login","dashboard","cadastro","sorteio"].forEach(t=>{
         document.getElementById(t).classList.add("hidden");
     });
     document.getElementById(id).classList.remove("hidden");
 }
 
-/* LOGIN (SÓ LOGIN AGORA) */
+/* LOGIN CORRETO (SEM CRIAR USUÁRIO) */
 window.login = async ()=>{
     try{
         await signInWithEmailAndPassword(auth, email.value, senha.value);
     }catch(err){
+        console.log(err);
 
         if(err.code === "auth/invalid-credential"){
             alert("Email ou senha incorretos");
-        }else if(err.code === "auth/user-not-found"){
+        } else if(err.code === "auth/user-not-found"){
             alert("Usuário não existe");
-        }else{
-            alert(err.message);
+        } else {
+            alert("Erro: " + err.code);
         }
-
-    }
-};
-
-/* CADASTRAR USUÁRIO (BOTÃO SEPARADO) */
-window.registrar = async ()=>{
-    try{
-        await createUserWithEmailAndPassword(auth, email.value, senha.value);
-        alert("Conta criada!");
-    }catch(err){
-        alert(err.message);
     }
 };
 
